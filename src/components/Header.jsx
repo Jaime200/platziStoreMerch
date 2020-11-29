21fb7ad1a5f9989e086e0acc95caf419ff36ad3d
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-11-29 10:34:06 -0600

    Componentes Header y Footer, Layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../style/components/Header.css'
+
+const Header = ()=>{
+    return(
+        <div className="Header">
+            <div className="Header-title">PlatziConf Merch</div>
+            <div className="Header checkout">
+                    checkout
+            </div>
+        </div>
+    )
+}
+
+export default Header
