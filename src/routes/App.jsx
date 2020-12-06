10f457d2943e7ada0ba8f32d5cd254453c6ca9a5
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-12-06 17:22:28 -0600

    Context, customHooks, Conexion de context

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,21 +9,25 @@ import NotFound from '../containers/NotFound'
 import Layout from '../components/Layout'
 // [BrowserRouter] Encapsula toda la navegacion de la aplicacion
 // [Switch] El primero que coincida con la ruta que se esta eligiendo
-
+import useInitialState from '../hooks/useInitialState'
+import AppContext from '../context/AppContext'
 const App = ()=>{
+  const initialState = useInitialState();
   return(
- <BrowserRouter> 
- <Layout>
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/checkout' component={Checkout}/>
-      <Route exact path='/checkout/information' component={Information}/>
-      <Route exact path='/checkout/payment' component={Payment}/>
-      <Route exact path='/checkout/success' component={Success}/>
-      <Route component={NotFound}/>
-    </Switch>
-  </Layout>
- </BrowserRouter>
+  <AppContext.Provider value={initialState}>
+    <BrowserRouter> 
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/checkout' component={Checkout}/>
+          <Route exact path='/checkout/information' component={Information}/>
+          <Route exact path='/checkout/payment' component={Payment}/>
+          <Route exact path='/checkout/success' component={Success}/>
+          <Route component={NotFound}/>
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+ </AppContext.Provider>
   )
 }
 
